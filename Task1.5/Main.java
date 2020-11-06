3fc1b6e0689f0d302bfd09e8334554fb55dab297
Наталья Галкина <73999088+Nata1305@users.noreply.github.com>
2020-11-06 21:05:08 +0300

    Create Main.java

---
+++ Task1.5/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();//TC
+        double b = 9./5;// (9/5)
+
+        System.out.println(a * b + 32); //TF = (9 / 5) * TC + 32
+
+    }
+}
