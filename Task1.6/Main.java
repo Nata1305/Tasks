67d3c2d78aaf3942ea8531046107b891f1591b5d
Наталья Галкина <73999088+Nata1305@users.noreply.github.com>
2020-11-06 21:17:43 +0300

    Create Main.java

---
+++ Task1.6/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        double b = 15./100;
+        System.out.println(a*b+a);
+
+    }
+}
