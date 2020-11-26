d790ffc95024e3acf5772dc08cf16ea7030a14f0
Наталья Галкина <73999088+Nata1305@users.noreply.github.com>
2020-11-26 12:46:02 +0300

    Update Main.java

--- task_5.3/Main.java
+++ task_5.3/Main.java
@@ -7,21 +7,22 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> nums = new ArrayList<>();
+        ArrayList<Integer> nums = new ArrayList ();
+
         nums.add(1);
         nums.add(4);
         nums.add(25);
         nums.add(100);
         nums.add(11);
-        ArrayList<Integer> nums2 = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            nums2.add(i);
-        }
-        nums.removeAll(nums2);
+        nums.add(8);
+        nums.add(6);
 
         System.out.println(nums);
-        }
+        ArrayList <Integer> newnums = new ArrayList<>();
+        nums.removeIf(integer -> (integer>0&&integer>10));
+
+       System.out.println(nums);
+    }
 
 }
-
 
